147ade9943d3473edeaa272919571a677bef6057
WillianRoosa <willian_roosa@icloud.com>
2025-07-21 10:31:21 -0300

    commit: estudo java intellij

--- src/test/java/EnumTest01.java
+++ src/test/java/EnumTest01.java
@@ -5,8 +5,10 @@ public class EnumTest01 {
     public static void main(String[] args) {
         Cliente cliente1 = new Cliente("Willian Rosa", TipoCliente.PESSOA_JURIDICA);
         Cliente cliente2 = new Cliente("Alex Rosa", TipoCliente.PESSOA_FISICA);
+        Cliente cliente3 = new Cliente("Edson Rosa", TipoCliente.PESSOA_FISICA);
 
         System.out.println(cliente1);
         System.out.println(cliente2);
+        System.out.println(cliente3);
     }
 }
