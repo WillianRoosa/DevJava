1577cf19ccc9b4961f3e7d11587a7cb415f010b1
WillianRoosa <willian_roosa@icloud.com>
2025-08-08 10:11:55 -0300

    commit: aprendendo Exceções no Java!

---
+++ src/main/java/projeto/java/javacore/exception/runtime/test/RuntimeExceptionTest03.java
@@ -0,0 +1,20 @@
+package projeto.java.javacore.exception.runtime.test;
+
+public class RuntimeExceptionTest03 {
+    public static void main(String[] args) {
+        abreConexao();
+    }
+
+    private static String abreConexao() {
+        try {
+            System.out.println("Abrindo um arquivo");
+            System.out.println("Escrevendo dados no arquivo");
+            return "Conexão aberta";
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("Fechaqndo recurso liberado pelo SO");
+        }
+        return null;
+    }
+}
