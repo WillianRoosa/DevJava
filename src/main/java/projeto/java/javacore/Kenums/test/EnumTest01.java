3df7cf6861a7302b865ae6067f5925bdf49018f8
WillianRoosa <willian_roosa@icloud.com>
2026-03-31 11:32:19 -0300

    commit atualização estudos.

--- src/main/java/projeto/java/javacore/Kenums/test/EnumTest01.java
+++ src/main/java/projeto/java/javacore/Kenums/test/EnumTest01.java
@@ -4,15 +4,24 @@ import projeto.java.javacore.Kenums.dominio.Cliente;
 import projeto.java.javacore.Kenums.dominio.TipoCliente;
 import projeto.java.javacore.Kenums.dominio.TipoPagamento;
 
+import java.text.NumberFormat;
+import java.util.Locale;
+
 public class EnumTest01 {
     public static void main(String[] args) {
+        Locale locale = new Locale("pt", "BR");
+        NumberFormat formatter = NumberFormat.getCurrencyInstance(locale);
+
         Cliente cliente1 = new Cliente("Willian Rosa", TipoCliente.PESSOA_JURIDICA);
         Cliente cliente2 = new Cliente("Alex Rosa", TipoCliente.PESSOA_FISICA);
 
         System.out.println(cliente1);
         System.out.println(cliente2);
-        System.out.printf("Calcular Desconto: %.2f\n", TipoPagamento.DEBITO.calcularDesconto(1000));
-        System.out.printf("Calcular Desconto: %.2f\n", TipoPagamento.CREDITO.calcularDesconto(1000));
+
+        String formatValue1 = formatter.format(TipoPagamento.DEBITO.calcularDesconto(1000));
+        String formatValue2 = formatter.format(TipoPagamento.CREDITO.calcularDesconto(1000));
+        System.out.printf("Calcular Desconto: %s\n", formatValue1);
+        System.out.printf("Calcular Desconto: %s\n", formatValue2);
 
         TipoCliente tipoCliente = TipoCliente.tipoClientePorNomeRelatorio("Pessoa Física");
         System.out.println(tipoCliente);
